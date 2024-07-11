aa7841409d0d3777a0874a86a06316438cb7dbd1
mahmoud khalil <mahmoud.khalil90@hotmail.com>
2024-07-11 23:45:35 +0300

    Dashboard still not completed

---
+++ src/pages/Academy.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Academy() {
+  return (
+    <h1>Academy</h1>
+  )
+}
+
+export default Academy;
---
+++ src/pages/eCommerce.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const eCommerce =() => {
+  return (
+    <div><h1>eCommerce</h1></div>
+    
+  )
+}
+
+export default eCommerce;
